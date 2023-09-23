a15b9817fbe67966beb3dd9d12b93c8a8bca2dca
apast0r <me@apast0r.com>
2023-09-23 20:11:12 +0200

    Implementate all IR send commands

--- components/climate_ir_YKRL/climate_ir_YKRL.cpp
+++ components/climate_ir_YKRL/climate_ir_YKRL.cpp
@@ -6,25 +6,46 @@ namespace climate_ir_YKRL {
 
 static const char *const TAG = "climate.climate_ir_YKRL";
 
+// Modify supported modes to quit HEAT_COOL
+climate::ClimateTraits YKRLClimate::traits(){
+    auto traits = ClimateIR::traits();
+    auto modes = traits.get_supported_modes();
+    modes.erase(climate::CLIMATE_MODE_HEAT_COOL);
+    traits.set_supported_modes(modes);
+    return traits;
+}
+
+// At this moment do nothing
+// Just sent AUTO ON or AUTO OFF
 void YKRLClimate::transmit_state() {
-    // At this moment do nothing
-    // Just sent AUTO ON or AUTO OFF
-    uint8_t remote_state[13] = { 0x3C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-    if (this->mode == climate::CLIMATE_MODE_OFF){
-        remote_state[1] = 0x07; 
-        remote_state[2] = 0xE0; 
-        remote_state[4] = 0xA0;
-        remote_state[11] = 0x05;
-        remote_state[12] = 0x4F; // checksum
-    } else {
-        //uint8_t remote_state[12] = { 0xc3, 0xe0, 0x07, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00, 0xa0 };
-        remote_state[1] = 0x07; 
-        remote_state[2] = 0xE0; 
-        remote_state[4] = 0xA0;
-        remote_state[9] = 0x20;
-        remote_state[11] = 0x05;
-        remote_state[12] = 0x6f; // checksum
+    uint16_t checksum = 0;
+    uint8_t remote_state[STATE_FRAME_SIZE] = {0};
+
+    // Byte0 - Set produc id
+    remote_state[0] = 0xC3;
+    // Byte1 - temperature + hswing
+    remote_state[1] = temperature_() + vswing_();
+    // Byte2 - vswing
+    remote_state[2] = hswing_();
+    // Byte3 - Temperature step .5
+    remote_state[3] = this->target_temperature == int(this->target_temperature) ? 0x00 : 0x80;
+    // Byte4 - Fan speed
+    remote_state[4] = fan_speed_();
+    // Byte5  - Extended fan modes
+    remote_state[5] = ext_fan_();
+    // Byte6 - mode
+    remote_state[6] = mode_();
+    // Byte 9 - ON/OFF
+    remote_state[9] = this->mode == climate::CLIMATE_MODE_OFF ? 0x00 : 0x20;
+    // Byte 11 - ¿Random=
+    remote_state[11] = 0x05;
+
+    //Calculate checksum
+    for (uint8_t i : remote_state){
+        checksum += i;
     }
+    remote_state[12] = checksum & 0xFF;
+
 
     //Prepare to transmit
     auto transmit = this->transmitter_->transmit();
@@ -36,7 +57,6 @@ void YKRLClimate::transmit_state() {
     data->space(LEAD_IN_SPACE);
 
     //Set data
-    // Iterate trough each frame
     bool bit;
     for (int i = 0; i < 13; i++){
         for (uint8_t mask = 1; mask >0; mask <<=1){ //swap bit trough mask
@@ -45,6 +65,8 @@ void YKRLClimate::transmit_state() {
             data->space(bit ? ONE_SPACE : ZERO_SPACE);
         }
     }
+
+    //Send LeadOut
     data -> mark(BIT_MARK);
     data -> space(LEAD_OUT_SPACE);
 
@@ -55,5 +77,83 @@ bool YKRLClimate::on_receive(remote_base::RemoteReceiveData data){
     //TODO
     return true;
 }
+
+uint8_t YKRLClimate::temperature_(){
+    switch(this->mode){
+        case climate::CLIMATE_MODE_FAN_ONLY:
+        case climate::CLIMATE_MODE_AUTO:
+            return 0x00;
+        default:
+            return ((uint8_t) roundf(this->target_temperature) - TEMP_OFFSET)<<3;
+    }
+}
+
+uint8_t YKRLClimate::vswing_(){
+    switch(this->swing_mode){
+        case climate::CLIMATE_SWING_HORIZONTAL:
+        case climate::CLIMATE_SWING_BOTH:
+            return VSWING_AUTO;
+        case climate::CLIMATE_SWING_OFF:
+        case climate::CLIMATE_SWING_VERTICAL:
+        default:
+            return VSWING_OFF;
+    }
+}
+
+uint8_t YKRLClimate::hswing_(){
+    switch(this->swing_mode){
+        case climate::CLIMATE_SWING_VERTICAL:
+        case climate::CLIMATE_SWING_BOTH:
+            return HSWING_AUTO;
+        case climate::CLIMATE_SWING_OFF:
+        case climate::CLIMATE_SWING_HORIZONTAL:
+        default:
+            return HSWING_OFF;
+    }
+}
+
+uint8_t YKRLClimate::fan_speed_(){
+    switch(this->fan_mode.value()){
+        case climate::CLIMATE_FAN_FOCUS:
+        case climate::CLIMATE_FAN_HIGH:
+            return 0x20;
+        case climate::CLIMATE_FAN_MEDIUM:
+            return 0x40;
+        case climate::CLIMATE_FAN_LOW:
+            return 0x60;
+        case climate::CLIMATE_FAN_QUIET:
+        case climate::CLIMATE_FAN_AUTO:
+        default:
+            return 0xA0;
+    }
+}
+
+uint8_t YKRLClimate::ext_fan_(){
+    switch(this->fan_mode.value()){
+        case climate::CLIMATE_FAN_FOCUS:
+            return 0x40;
+        case climate::CLIMATE_FAN_QUIET:
+            return 0x80;
+        default:
+            return 0x00;
+    }
+}
+
+uint8_t YKRLClimate::mode_(){
+    switch(this->mode){
+        case climate::CLIMATE_MODE_COOL:
+            return 0x20;
+        case climate::CLIMATE_MODE_DRY:
+            return 0x40;
+        case climate::CLIMATE_MODE_HEAT:
+            return 0x80;
+        case climate::CLIMATE_MODE_FAN_ONLY:
+            return 0xC0;
+        case climate::CLIMATE_MODE_AUTO:
+        default:
+            return 0x00;
+    }
+}
+
 }  // namespace climate_ir_YKRL
 }  // namespace esphome
--- components/climate_ir_YKRL/climate_ir_YKRL.h
+++ components/climate_ir_YKRL/climate_ir_YKRL.h
@@ -8,9 +8,10 @@ namespace climate_ir_YKRL {
 // Temperature
 const uint8_t TEMP_MIN = 16;  // Celsius
 const uint8_t TEMP_MAX = 32;  // Celsius
+const uint8_t STATE_FRAME_SIZE = 13;  // Number of bytes to send
+const uint8_t TEMP_OFFSET = 8;  // Temperature offset to get same bits as in ir code
 
 const uint16_t CARRIER_FRECUENCY = 38000;
-
 const uint16_t LEAD_IN_MARK = 9020;
 const uint16_t LEAD_IN_SPACE = 4520;
 const uint16_t LEAD_OUT_SPACE = 50000; // Message space
@@ -18,20 +19,35 @@ const uint16_t BIT_MARK = 552;
 const uint16_t ZERO_SPACE = 657;
 const uint16_t ONE_SPACE = 1735;
 
+const uint8_t VSWING_OFF = 0x07;
+const uint8_t VSWING_AUTO = 0x03;
+const uint8_t HSWING_OFF = 0x00;
+const uint8_t HSWING_AUTO = 0xE0;
+
+
 class YKRLClimate : public climate_ir::ClimateIR {
     public:
         YKRLClimate()
             : climate_ir::ClimateIR(TEMP_MIN, TEMP_MAX, 0.5f, true, true,
-                                    {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH},
-                                    {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL},
+                                    {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_QUIET, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH},
+                                    {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH},
                                     {climate::CLIMATE_PRESET_ECO, climate::CLIMATE_PRESET_BOOST, climate::CLIMATE_PRESET_SLEEP, climate::CLIMATE_PRESET_COMFORT}) {}
     protected:
+        /// Set the supported modes
+        climate::ClimateTraits traits() override;
         /// Transmit via IR the state of this climate controller.
         void transmit_state() override;
         /// Handle received IR Buffer
         bool on_receive(remote_base::RemoteReceiveData data) override;
 
-        // void calc_checksum_(uint32_t &value);
+        /// Calculate values depending of the mode,temp,etc...
+        uint8_t temperature_();
+        uint8_t fan_speed_();
+        uint8_t operation_mode_();
+        uint8_t vswing_();
+        uint8_t hswing_();
+        uint8_t ext_fan_();
+        uint8_t mode_();
 
         climate::ClimateMode mode_before_{climate::CLIMATE_MODE_OFF};
 };
